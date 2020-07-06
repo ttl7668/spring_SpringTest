891de440a9768a9273d07f5ab86d6397c5e10da2
ttl7668 <ttl7668@naver.com>
2020-07-06 17:50:49 +0900

    spring55

---
+++ src/main/java/com/simple/controller/Main.java
@@ -0,0 +1,16 @@
+package com.simple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Main {
+
+	
+	@RequestMapping("/")
+	public String hello() {
+
+		return "hello";
+	}
+
+}
